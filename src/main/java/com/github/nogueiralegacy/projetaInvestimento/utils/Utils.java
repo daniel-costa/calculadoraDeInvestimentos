449e61324fd904094692331ae6e23ac5a8e93e24
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-04-05 16:00:49 -0300

    metodo util desnecessario e pom sem a propriedade do nome do projeto

--- src/main/java/com/github/nogueiralegacy/projetaInvestimento/utils/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.github.nogueiralegacy.projetaInvestimento.utils;
-
-public class Utils {
-    public static double parseDouble(String valor) {
-        valor.replace(",", ".");
-        return Double.parseDouble(valor);
-    }
-}
