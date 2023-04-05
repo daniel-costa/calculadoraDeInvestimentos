a3044dae3e446fab06a95e35e55bed6ce4804189
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-04-05 17:05:48 -0300

    Testes da aplicação

---
+++ src/test/java/com/github/nogueiralegacy/projetaInvestimento/domain/TestCalculadoraInvestimento.java
@@ -0,0 +1,47 @@
+package com.github.nogueiralegacy.projetaInvestimento.domain;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCalculadoraInvestimento {
+    @Test
+    public void testMontateFinal0Meses() {
+        double investimentoInicial = 1000;
+        double investimentoMensal = 100;
+        int quantidadeMeses = 0;
+        double porcentagemRendimentoMensal = 1;
+        double resultado = CalculadoraInvestimento.montanteFinal(investimentoInicial, investimentoMensal, quantidadeMeses, porcentagemRendimentoMensal);
+
+        double resultadoEsperado = 1000;
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    public void testMontateFinal1Mes() {
+        double investimentoInicial = 1000;
+        double investimentoMensal = 100;
+        int quantidadeMeses = 1;
+        double porcentagemRendimentoMensal = 1;
+        double resultado = CalculadoraInvestimento.montanteFinal(investimentoInicial, investimentoMensal, quantidadeMeses, porcentagemRendimentoMensal);
+
+        double resultadoEsperado = 1010;
+
+        assertEquals(resultadoEsperado, resultado);
+    }
+
+    @Test
+    public void testMontateFinal3Meses() {
+        double investimentoInicial = 1000;
+        double investimentoMensal = 100;
+        int quantidadeMeses = 3;
+        double porcentagemRendimentoMensal = 1;
+        double resultado = CalculadoraInvestimento.montanteFinal(investimentoInicial, investimentoMensal, quantidadeMeses, porcentagemRendimentoMensal);
+        double resultadoFormatado = Math.round(resultado * 1000.0) / 1000.0;
+
+        double resultadoEsperado = 1233.311;
+
+        assertEquals(resultadoEsperado, resultadoFormatado);
+    }
+
+}
