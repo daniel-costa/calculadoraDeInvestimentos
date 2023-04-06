845803629e99e962effbe34a9690b43d08f160d8
nogueiralegacy <daniel.nogueira.dacosta@gmail.com>
2023-04-06 15:09:18 -0300

    implementação da feture de correcao anual no metodo que projeta os investimentos

--- src/main/java/com/github/nogueiralegacy/projetaInvestimento/domain/CalculadoraInvestimento.java
+++ src/main/java/com/github/nogueiralegacy/projetaInvestimento/domain/CalculadoraInvestimento.java
@@ -3,24 +3,40 @@ package com.github.nogueiralegacy.projetaInvestimento.domain;
 public class CalculadoraInvestimento {
 
     static public double montanteFinal(double investimentoInicial, double investimentoMensal, int quantidadeMeses,
-            double porcentagemRendimentoMensal) {
+            double porcentagemRendimentoMensal, double correcaoAnualInvestimentoMensal) {
         double resultado = investimentoInicial * Math.pow((1 + porcentagemRendimentoMensal / 100), quantidadeMeses);
 
         // projeta cada investimento mensal pelos meses que ele sera aplicado
+        int count = 0;
         for (int indice = quantidadeMeses - 1; indice > 0; indice--) {
-            resultado += investimentoMensal * Math.pow((1 + porcentagemRendimentoMensal / 100), indice);
+            resultado += investimentoMensal * Math.pow((1 + (porcentagemRendimentoMensal / 100)), indice);
+            count++;
+            if (count % 12 == 0) {
+                investimentoMensal *= (1 + (correcaoAnualInvestimentoMensal / 100));
+            }
         }
 
         return resultado;
     }
 
-    static public double totalInvestido(double investimentoInicial, double investimentoMensal, int quantidadeMeses) {
+    static public double totalInvestido(double investimentoInicial, double investimentoMensal,
+                                        int quantidadeMeses, double correcaoAnualInvestimentoMensal) {
         double resultado = investimentoInicial;
         if (quantidadeMeses == 0) {
             return resultado;
         }
 
-        resultado += investimentoMensal * (quantidadeMeses - 1);
+
+        int anos = (int) Math.floor(quantidadeMeses / 12);
+        int mesesRemanescentes = (quantidadeMeses - 1) % 12;
+
+        for (int i = 0; i < anos; i++) {
+            resultado += investimentoMensal  * 12;
+            investimentoMensal *= (1 + (correcaoAnualInvestimentoMensal / 100));
+        }
+
+        resultado += investimentoMensal * mesesRemanescentes;
+
         return resultado;
     }
 }
